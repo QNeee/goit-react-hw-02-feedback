131592d8644139383c7f71fdda9d0733bdb1d14d
QNeee <vovasagan7@gmail.com>
2022-11-30 06:54:37 +0300

    dsdsa

--- src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/Feedback/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,3 +1,7 @@
 import styled from "@emotion/styled";
 export const Button = styled.button`height:40px;margin-right:15px;width:150px;text-transform:upperCase;
-background-color:green;cursor:pointer;`;
+background-color:green;cursor:pointer;
+:hover{
+    background-color:blue;
+    color:yellow;
+}`;
